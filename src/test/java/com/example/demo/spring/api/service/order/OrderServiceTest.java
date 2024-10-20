32f0bcce690589f9b2ddfe97b89c1ec221f30684
woonie155 <woonie155@gmail.com>
2024-10-21 03:44:54 +0900

    주문: 단위 테스트, business layer 통합 테스트

---
+++ src/test/java/com/example/demo/spring/api/service/order/OrderServiceTest.java
@@ -0,0 +1,68 @@
+package com.example.demo.spring.api.service.order;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.demo.spring.api.controller.order.request.OrderCreateRequest;
+import com.example.demo.spring.api.service.order.response.OrderResponse;
+import com.example.demo.spring.domain.product.Product;
+import com.example.demo.spring.domain.product.ProductRepository;
+import com.example.demo.spring.domain.product.ProductSellingStatus;
+import com.example.demo.spring.domain.product.ProductType;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.assertj.core.groups.Tuple;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class OrderServiceTest {
+
+    @Autowired private ProductRepository productRepository;
+    @Autowired private OrderService orderService;
+
+    @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
+    @Test
+    void createOrder(){
+        //given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Product product1 = createProduct(ProductType.HANDMADE, "001", 1000);
+        Product product2 = createProduct(ProductType.HANDMADE, "002", 3000);
+        Product product3 = createProduct(ProductType.HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "002"))
+                .build();
+
+
+        //when
+        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+
+        //then
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registeredDateTime, 4000);
+        assertThat(orderResponse.getProducts()).hasSize(2)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        Tuple.tuple("001", 1000),
+                        Tuple.tuple("002", 3000)
+                );
+    }
+
+    private Product createProduct(ProductType type, String productNumber, int price){
+        return Product.builder()
+                .type(type)
+                .productNumber(productNumber)
+                .price(price)
+                .sellingStatus(ProductSellingStatus.SELLING)
+                .name("메뉴이름")
+                .build();
+    }
+}
