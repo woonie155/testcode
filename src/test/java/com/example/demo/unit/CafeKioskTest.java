bca31b8c6870d47607544e85418da0c0447391f1
woonie155 <woonie155@gmail.com>
2024-10-20 23:19:45 +0900

    주문 총 가격 테스트

--- src/test/java/com/example/demo/unit/CafeKioskTest.java
+++ src/test/java/com/example/demo/unit/CafeKioskTest.java
@@ -69,6 +69,19 @@ class CafeKioskTest {
     }
 
     @Test
+    void calculateTotalPrice(){
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        Latte latte = new Latte();
+
+        cafeKiosk.add(americano);
+        cafeKiosk.add(latte);
+
+        int totalPrice = cafeKiosk.calculateTotalPrice();
+        assertThat(totalPrice).isEqualTo(8500);
+    }
+
+    @Test
     void createOrderWithCurrentTime(){
         CafeKiosk cafeKiosk = new CafeKiosk();
         Americano americano = new Americano();
