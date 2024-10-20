88216ecd0d264b46b1e31b203610e95dcbb1b9d7
woonie155 <woonie155@gmail.com>
2024-10-21 04:46:48 +0900

    fix: 주문 API 응답 수정

--- src/main/java/com/example/demo/spring/api/controller/order/OrderController.java
+++ src/main/java/com/example/demo/spring/api/controller/order/OrderController.java
@@ -2,6 +2,7 @@ package com.example.demo.spring.api.controller.order;
 
 import com.example.demo.spring.api.controller.order.request.OrderCreateRequest;
 import com.example.demo.spring.api.service.order.OrderService;
+import com.example.demo.spring.api.service.order.response.OrderResponse;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,9 +16,9 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/api/v1/orders/new")
-    public void createOrder(@RequestBody OrderCreateRequest request) {
+    public OrderResponse createOrder(@RequestBody OrderCreateRequest request) {
         LocalDateTime registeredDateTime = LocalDateTime.now();
-        orderService.createOrder(request, registeredDateTime);
+        return orderService.createOrder(request, registeredDateTime);
     }
 
 }
